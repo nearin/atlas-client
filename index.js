955b58d58a387112af29e8bc0c76be5b9d8158c3
pontusn <pontusn@kth.se>
2015-10-19 19:40:29 +0200

    Added socket.io

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 export function hello(name) {
   return 'Hello ' + name;
 }
+
+import {hello} from 'nearin/atlas-client';
+
+import {io} from 'socket.io/socket.io.js'
+
+export function connect() {
+  var socket = io();
+}
