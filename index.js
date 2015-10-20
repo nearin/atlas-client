e6d3cb1f26b90d97d48b02315d4d8bc7c908ccc3
pontusn <pontusn@kth.se>
2015-10-20 09:46:43 +0200

    Added socket.io

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import io from 'npm/socket.io/socket.io-client';
-
 export function hello(name) {
   return 'Hello ' + name;
 }
 
 
 export function connect() {
+
+  import io from 'npm/socket.io/socket.io-client';
+
   var socket = io();
 }
